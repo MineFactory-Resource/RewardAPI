f42350313a90819b5c9f64966649417266f29e1b
LesserJang <jih10157@naver.com>
2022-02-02 02:33:50 +0900

    보관함 메뉴 리팩토링

--- src/main/java/net/teamuni/rewardapi/menu/StorageBoxMenu.java
+++ src/main/java/net/teamuni/rewardapi/menu/StorageBoxMenu.java
@@ -1,29 +1,31 @@
 package net.teamuni.rewardapi.menu;
 
 import com.google.common.collect.Lists;
-import com.google.common.reflect.TypeToken;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import net.teamuni.rewardapi.RewardAPI;
+import net.teamuni.rewardapi.config.ConfigManager;
+import net.teamuni.rewardapi.config.MessageStorage;
+import net.teamuni.rewardapi.data.PlayerDataManager;
 import net.teamuni.rewardapi.data.PlayerDataManager.PlayerData;
 import net.teamuni.rewardapi.data.object.CommandReward;
 import net.teamuni.rewardapi.data.object.ItemReward;
 import net.teamuni.rewardapi.data.object.Reward;
-import net.teamuni.rewardapi.config.ConfigManager;
-import net.teamuni.rewardapi.config.MessageStorage;
-import net.teamuni.rewardapi.config.SimpleItemStack;
-import net.teamuni.rewardapi.data.PlayerDataManager;
-import org.spongepowered.api.Sponge;
-import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.entity.living.player.User;
-import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.item.inventory.ItemStackSnapshot;
-import org.spongepowered.api.item.inventory.Slot;
-import org.spongepowered.api.item.inventory.entity.UserInventory;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class StorageBoxMenu extends Menu {
 
@@ -31,7 +33,7 @@ public class StorageBoxMenu extends Menu {
     private static String title;
     private static int rows;
     private static int countReward;
-
+    private static final Inventory tempInv = Bukkit.createInventory(null, 36);
     private final UUID uuid;
     private int page = 1;
 
@@ -45,11 +47,11 @@ public class StorageBoxMenu extends Menu {
     public static void init() {
         ConfigManager menuConfig = RewardAPI.getInstance().getMenuConfig();
 
-        title = menuConfig.getString("", "storagebox", "title");
-        rows = menuConfig.getValue(Integer.class, 6, "storagebox", "rows");
+        title = menuConfig.getString("", "StorageBox.title");
+        rows = menuConfig.getValue(Integer.class, 6, "StorageBox.rows");
 
-        String pattern = String.join("", menuConfig.getValue(new TypeToken<List<String>>() {}, "storagebox", "pattern")
-                .orElse(Collections.emptyList()));
+        String pattern = String.join("",
+            menuConfig.getValueList(String.class, "StorageBox.Pattern"));
         try {
             pattern = pattern.substring(0, rows * 9);
         } catch (StringIndexOutOfBoundsException ignored) {}
@@ -57,28 +59,25 @@ public class StorageBoxMenu extends Menu {
         menuPattern = new MenuPattern(pattern);
         countReward = countChar(pattern, '_');
 
-        Map<Character, SimpleItemStack> map = menuConfig.getValue(
-            new TypeToken<Map<Character, SimpleItemStack>>() {},
-            "storagebox", "items").orElse(Collections.emptyMap());
+        Map<Character, ItemStack> map = menuConfig.getMapSimpleItemStack("StorageBox.Items");
         map.entrySet().stream()
             .filter(entry ->
                 entry.getKey() != ' ' && entry.getKey() != '_' &&
-                entry.getKey() != 'L' && entry.getKey() != 'R')
-            .forEach(entry -> menuPattern.setItem(entry.getKey(),
-                entry.getValue().createItemStackSnapShot()));
+                    entry.getKey() != 'L' && entry.getKey() != 'R')
+            .forEach(entry -> {
+                menuPattern.setItem(entry.getKey(),
+                    entry.getValue());
+            });
 
-        List<ItemStackSnapshot> issList = Lists.newArrayListWithExpectedSize(4);
-        String[][] nodePath = new String[][] {
-            new String[] {"storagebox", "button", "left", "can"},
-            new String[] {"storagebox", "button", "left", "cant"},
-            new String[] {"storagebox", "button", "right", "can"},
-            new String[] {"storagebox", "button", "right", "cant"}
+        List<ItemStack> issList = Lists.newArrayListWithExpectedSize(4);
+        String[] paths = new String[]{
+            "StorageBox.Buttons.Left.Can",
+            "StorageBox.Buttons.Left.Cant",
+            "StorageBox.Buttons.Right.Can",
+            "StorageBox.Buttons.Right.Cant"
         };
-        for (String[] node : nodePath) {
-            issList.add(menuConfig.getValue(
-                    new TypeToken<SimpleItemStack>() {}, node)
-                .orElse(SimpleItemStack.NONE)
-                .createItemStackSnapShot());
+        for (String path : paths) {
+            issList.add(menuConfig.getSimpleItemStack(path).orElse(new ItemStack(Material.AIR)));
         }
         menuPattern.setTurningButtons(issList);
     }
@@ -105,7 +104,7 @@ public class StorageBoxMenu extends Menu {
         menuPattern.updateReward(this,
             rewards.subList(start, end)
                 .stream()
-                .map(reward -> reward.getViewItem().createStack())
+                .map(Reward::getViewItem)
                 .collect(Collectors.toList()));
 
         menuPattern.updateTurningButton(this,
@@ -114,11 +113,8 @@ public class StorageBoxMenu extends Menu {
     }
 
     @Override
-    protected void onClick(Player player, int slotIndex, Slot slot,
-        ItemStackSnapshot clickedItem, ClickType clickType) {
-        if (clickedItem.isEmpty()) {
-            return;
-        }
+    protected void onClick(Player player, int slotIndex, ItemStack clickItem,
+        InventoryAction clickType) {
         String pattern = menuPattern.getPattern();
         char c = pattern.charAt(slotIndex);
         if (c != '_' && c != 'L' && c != 'R') {
@@ -144,24 +140,17 @@ public class StorageBoxMenu extends Menu {
             Reward reward = rewards.get(rewardIndex);
             if (reward.isItemReward()) {
                 ItemReward itemReward = (ItemReward) reward;
-                List<ItemStack> items = new ArrayList<>();
-                for (ItemStackSnapshot iss : itemReward.getRewardItems()) {
-                    items.add(iss.createStack());
-                }
-                if (items.stream().allMatch(item ->
-                    ((UserInventory<? extends User>) player.getInventory()).getMain().canFit(item))) {
-                    for (ItemStack item : items) {
-                        player.getInventory().offer(item);
-                    }
+                if (canFit(player, itemReward.getRewardItems())) {
+                    player.getInventory().addItem(itemReward.getRewardItems());
                 } else {
                     MessageStorage messageStorage = RewardAPI.getInstance().getMessageStorage();
-                    player.sendMessage(messageStorage.getMessage("menu", "out_of_space"));
+                    player.sendMessage(messageStorage.getMessage("Menu.out_of_space"));
                     return;
                 }
             } else {
                 CommandReward commandReward = (CommandReward) reward;
                 for (String command : commandReward.getCommands()) {
-                    Sponge.getCommandManager().process(Sponge.getServer().getConsole(), command);
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
                 }
             }
             playerData.removeReward(rewardIndex);
@@ -169,9 +158,7 @@ public class StorageBoxMenu extends Menu {
         } else {
             return;
         }
-        Sponge.getScheduler().createTaskBuilder()
-            .execute(this::update)
-            .submit(RewardAPI.getInstance());
+        Bukkit.getScheduler().runTask(RewardAPI.getInstance(), this::update);
     }
 
     private int getRewardIndex(int slotIndex) {
@@ -188,5 +175,18 @@ public class StorageBoxMenu extends Menu {
             }
         }
         return (page - 1) * countReward + tmp - 1;
+    }
+
+    public static boolean canFit(Player p, ItemStack[] items) {
+        PlayerInventory pInv = p.getInventory();
+        for (int i = 0; i < 36; i++) {
+            ItemStack item = pInv.getItem(i);
+            if (item != null) {
+                tempInv.setItem(i, item.clone());
+            }
+        }
+        Map<Integer, ItemStack> map = tempInv.addItem(Arrays.stream(items).map(ItemStack::clone).toArray(ItemStack[]::new));
+        tempInv.clear();
+        return map.isEmpty();
     }
 }
